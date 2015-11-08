63956228e4755dc9e935bcd39d0033a6175dad01
arnoud ebben <ebbenarnoud@gmail.com>
2015-11-08 17:52:01 +0100

    standaard template met git

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync').create();
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS    = require('gulp-minify-css');
+var sourcemaps   = require('gulp-sourcemaps');
+var plumber      = require('gulp-plumber');
+var concat       = require('gulp-concat');
+var uglify       = require('gulp-uglify');
+
+// Static Server
+gulp.task('server', ['styles'], function() {
+
+	browserSync.init({
+		proxy: 'localhost/testwebsite'
+	});
+
+	gulp.watch('src/sass/**/*.scss', ['styles']).on('change', browserSync.reload);
+	gulp.watch('src/js/**/*.js', ['js']).on('change', browserSync.reload);
+	gulp.watch('*.php').on('change', browserSync.reload);
+});
+
+// Styles
+gulp.task('styles', function() {
+	return gulp.src('src/sass/**/*.scss')
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+
+		.pipe(sass())
+		.pipe(concat('main.css'))
+		.pipe(autoprefixer())
+		.pipe(minifyCSS())
+
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest('assets/css'))
+		.pipe(browserSync.stream());
+});
+
+// JS
+gulp.task('js', function() {
+	return gulp.src('src/js/**/*.js')
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+
+		.pipe(uglify())
+		.pipe(concat('main.js'))
+
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest('assets/js'))
+		.pipe(browserSync.stream());
+});
+
+// Default
+gulp.task('default', ['server']);
---
+++ header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/assets/css/main.css" media="screen" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
+	
+	<?php wp_head(); ?>
+
+</head>
+<body>
